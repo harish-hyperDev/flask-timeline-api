e6bc69e2a44d7af40768bba2330323f536efb385
harish-hyperDev <coolprogrammer.777@gmail.com>
2023-05-30 20:52:49 +0530

    changing structure

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
-from timeline.app import app
+import pandas as pd
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    df = pd.read_csv('filename.csv')
+    data = df.to_json(orient='records')
+    
+    print(data)
+    return render_template('index.html')
+
+@app.route('/<opp_id>/<type>', methods=['GET', 'POST'])
+def home():
+    df = pd.read_csv('filename.csv')
+    data = df.to_json(orient='records')
+    return render_template('index.html')
+
+@app.route('/test', methods=['GET'])
+def test():
+    return 'test'
+
+@app.route('/<id>', methods=['GET', 'POST'])
+def api_id(id):
+    return render_template('index.html', id = id)
+
+
 
 if __name__ == "__main__":
     app.run()
--- timeline/app.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-import timeline.views
-
-# app.register_blueprint(users, url_prefix='/home')
